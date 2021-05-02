567ce921ad5c52d8cb97dd0853b724e4a2401ac2
Oscar Alvarez <oalvarezr@unal.edu.co>
2021-05-01 19:06:22 -0500

    falta finalizar Register.js y terminar las graficas

--- src/views/Auth/Register/Register.js
+++ src/views/Auth/Register/Register.js
@@ -1,3 +1,42 @@
+import axios from 'axios';
+
 export default {
-  name: 'Register'
-}
+  name: 'Register',
+  data() {
+    return {
+      alert1: false,
+      model: {
+        Firstname: '',
+        Lastname: '',
+        Email: '',
+        Password: '',
+        Passwordconfirm: ''
+      },
+    };
+  },
+  methods: {
+    postData() {
+      if(this.model.Password == this.model.Passwordconfirm){
+        this.alert1 = false;
+        axios
+          .post('graphql',{
+            query: "{getAllDriver{driversLicense name driverExperience averageDriverRating}}"
+          })
+          .then((res) => {
+            console.log('Send register completed');
+            console.log(res.data);
+          })
+          .catch((err) => {
+            console.log('Error Register');
+            console.log(err)
+          });
+      }else{
+        this.alert1 = true;
+      }
+    },
+    prueba(){
+      console.log(this.model);
+      this.postData();
+    }
+  },
+};
