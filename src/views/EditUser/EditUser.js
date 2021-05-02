75142ea26feeaab15dfdfa7e140053e46dc609b9
ValentinaViafara <vviafarae@unal.edu.co>
2021-05-02 15:11:41 -0500

    edit users

--- src/views/EditUser/EditUser.js
+++ src/views/EditUser/EditUser.js
@@ -16,12 +16,15 @@ export default {
       },
     };
   },
+
+  created(){
+    if (this.User_id) {
+      this.getData();
+    }
+  },
+
   methods: {
-    created(){
-      if (this.User_id) {
-        this.getData();
-      }
-    },
+    
     getData() {
       axios
       .post('graphql', {
@@ -36,17 +39,41 @@ export default {
       })
         .then((res) => {
           console.log(res.data.data.getuser);
-          this.model = res.data.data.getuser
+          this.model.User_id = res.data.data.getuser.User_id;
+          this.model.Firstname = res.data.data.getuser.Firstname;
+          this.model.Lastname= res.data.data.getuser.Lastname;
+          this.model.Email = res.data.data.getuser.Email;
         })
         .catch((err) => {
-          
+          console.log(err);
         });
     },
+
     sendEdit() {
-      console.log('Save Edit');
+      axios
+        .post('graphql', {
+          query: `mutation{updateUser(id: ${this.model.User_id}, user:{User_id: ${this.model.User_id},Firstname: "${this.model.Firstname}", Lastname: "${this.model.Lastname}", Email: "${this.model.Email}", User_password: "${this.model.User_password}", Block_account: ${this.model.Block_account}}){User_id}}`,
+        })
+        .then((res) => {
+          console.log(res.data)
+          this.$router.push('/admin/users');
+        })
+        .catch((err) => console.log(err));
     },
     sendDelete() {
-      console.log('Delete');
+      axios
+        .post('graphql',{
+          query: `mutation{
+            deleteUser(id: ${this.model.User_id}){
+              success
+            }
+          }`
+        })
+        .then((res) => {
+          console.log(res.data)
+          this.$router.push('/admin/drivers');
+        })
+        .catch((err) => console.log(err));
     },
   },
 };
