81e542c85b7f940231f4fda09df62d72e8021bc4
Oscar Alvarez <oalvarezr@unal.edu.co>
2021-08-21 12:09:35 -0500

    finish token

--- src/main.js
+++ src/main.js
@@ -14,6 +14,28 @@ Vue.config.performance = true
 Vue.use(CoreuiVue)
 Vue.prototype.$log = console.log.bind(console)
 
+router.beforeEach((to, from, next) => {
+  if(to.fullPath == "/register" || to.fullPath == "/login"){
+    next()
+  }else{
+    axios
+      .post('graphql', {
+        query:
+          `mutation{put_token(id: ${store.state.user_id}, token: "${store.state.token}"){isValid }}`,
+      })
+      .then((res) => {
+        if(res.data.data.put_token.isValid){
+          next()
+        }else{
+          next('/login')
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+  }
+})
+
 new Vue({
   el: '#app',
   router,
--- src/views/Auth/Login/Login.js
+++ src/views/Auth/Login/Login.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import store from '../../../store'
 export default {
   name: 'Login',
   data() {
@@ -16,14 +17,29 @@ export default {
       axios
         .post('graphql', {
           query:
-            `{get_login(email: "${this.model.email}", password: "${this.model.password}"){isLogin}}`,
+            `{get_login(email: "${this.model.email}", password: "${this.model.password}"){isLogin user_id}}`,
         })
         .then((res) => {
-          if(res.data.data.get_login == 1){
-
+          if(res.data.data.get_login.isLogin == 1){
+            var user = res.data.data.get_login.user_id
+            axios
+              .post('graphql', {
+                query:
+                  `mutation{post_token(id: ${user},type: "Login"){token}}`,
+              })
+              .then((res) => {
+                store.commit('updateUser',user);
+                store.commit('updateToken',res.data.data.post_token.token);
+                console.log(store.state);
+                this.$router.push('/admin');
+              })
+              .catch((err) => {
+                console.log(err)
+              });
           }
         })
         .catch((err) => {
+          console.log(err)
           this.alert1 = true
         });
     },
